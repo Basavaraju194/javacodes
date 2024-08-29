215d8cae8b8d028068ef90a96b4950efb03798b0
Basavaraju194 <143798205+Basavaraju194@users.noreply.github.com>
2024-08-30 00:16:50 +0530

    java code

---
+++ PrintStatements.java
@@ -0,0 +1,26 @@
+package com.printStatement;
+
+public class PrintStatements {
+
+	static String name = "basavaraju";
+	static int rollnumber = 25;
+
+	public static void main(String[] args) {
+		
+		System.out.println("Print output");
+		System.out.print("Hi How are you ");
+		System.out.print("Basavaraju");
+
+		System.out.println("Println output");
+		System.out.println("Hi Hi How are you");
+		System.out.println("Basavaraju");
+
+		
+		System.out.println("Println output");
+		System.out.printf("the current year is %d and current month is %s and current Date is %d " ,2023,"October",20);
+		System.out.printf("the current year is %d %n current month is %s %n current Date is %d /n" ,2023,"October",20);
+		System.out.printf(" the current year is %d %n current month is %s %n current Date is %d " ,2023,"October",20);
+
+	}
+
+}
